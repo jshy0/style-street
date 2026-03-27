bdc34f09518d3c3b004ac490cb3613f4056b6b7d
Josh Harris <josh.harris@ambrit.com>
2026-03-27 22:54:59 +0000

    chore: remove old products scaffold data

--- app/data/products.ts
+++ app/data/products.ts
@@ -15,33 +15,3 @@ export interface Product {
   badge?: "New" | "Hot";
   category: Categories;
 }
-
-export const products: Product[] = [
-  {
-    id: 1,
-    name: "Black Washed Hoodie",
-    slug: "black-washed-hoodie",
-    price: 60,
-    image: "/img/black-washed-hoodie.webp",
-    badge: "New",
-    category: "Hoodies",
-  },
-  {
-    id: 2,
-    name: "Green Washed Hoodie",
-    slug: "green-washed-hoodie",
-    price: 60,
-    image: "/img/green-washed-hoodie.webp",
-    badge: "New",
-    category: "Hoodies",
-  },
-  {
-    id: 3,
-    name: "Black Washed Tee",
-    slug: "black-washed-tee",
-    price: 30,
-    image: "/img/black-washed-tee.webp",
-    badge: "New",
-    category: "Tops",
-  },
-];
