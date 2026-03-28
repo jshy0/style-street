2e109d18f8d53122ec45419bfbdb8ac2f77f6e82
Josh Harris <josh.harris@ambrit.com>
2026-03-28 09:53:50 +0000

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import { Navbar } from "@/components/Navbar";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/next";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 const orbitron = Orbitron({
   variable: "--font-orbitron",
@@ -32,12 +33,19 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", orbitron.variable, spaceGrotesk.variable, "font-sans", geist.variable)}
+      className={cn(
+        "h-full",
+        orbitron.variable,
+        spaceGrotesk.variable,
+        "font-sans",
+        geist.variable,
+      )}
     >
       <body className="min-h-full flex flex-col bg-[#111012] text-zinc-100">
         <Navbar />
         {children}
         <Toaster theme="dark" />
+        <Analytics />
       </body>
     </html>
   );
